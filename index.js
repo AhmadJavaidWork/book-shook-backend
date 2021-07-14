2916420eadc17e1d8f0036488b0410e690e47aa4
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2021-07-14 17:05:05 +0500

    added start script and rearranged server

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-import { port } from './config/environment';
-import app from './app';
+require('@babel/register');
 
-const start = async () => {
-  try {
-    await app.listen(port);
-    console.log(`🚀  GraphQL server running at port: ${port}`);
-  } catch {
-    console.log('Not able to run GraphQL server');
-  }
-};
-
-start();
+exports = module.exports = require('./server');
