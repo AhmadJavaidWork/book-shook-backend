6a784b9eb4f2b7c3d24b0a6e24263b402cac3813
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2021-07-15 18:01:42 +0500

    code cleaning

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ import compression from 'compression';
 import { graphqlHTTP } from 'express-graphql';
 import schema from './schema';
 import knex from './knex';
-import {
-  NODE_ENV,
-  FRONTEND_PROD_URL,
-  FRONTEND_DEV_URL,
-} from './config/environment';
+import { NODE_ENV, FRONTEND_URL } from './config/environment';
 
 const app = express();
 
@@ -19,16 +15,8 @@ if (NODE_ENV === 'production' || NODE_ENV === 'development') {
   app.use(compression());
   app.use(morgan('dev'));
 }
-if (NODE_ENV === 'production') {
-  app.use(
-    cors({
-      credentials: true,
-      origin: FRONTEND_PROD_URL,
-    })
-  );
-} else {
-  app.use(cors({ credentials: true, origin: FRONTEND_DEV_URL }));
-}
+
+app.use(cors({ credentials: true, origin: FRONTEND_URL }));
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
 
